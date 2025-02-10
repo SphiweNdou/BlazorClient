8581236ff0ec576355837430cb895d5a4336c7a6
Sphiwe Ndou <sphiwe.ndou@siemens.com>
2025-02-10 15:12:23 +0200

    Add TokenStorageHelper

---
+++ BlazorClient/Helpers/TokenStorageHelper.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.JSInterop;
+using System.Threading.Tasks;
+
+namespace BlazorClient.Helpers
+{
+    public static class TokenStorageHelper
+    {
+        private static IJSRuntime _jsRuntime;
+
+        public static void  Initialise(IJSRuntime jSRuntime)
+        {
+            _jsRuntime = jSRuntime;
+        }
+
+        public static async Task SetToken(string token)
+        {
+            await _jsRuntime.InvokeVoidAsync("localStorage.setItem", "authToken", token);
+        }
+
+        public static async Task<string> GetToken()
+        {
+            return await _jsRuntime.InvokeAsync<string>("localStorage.getItem", "authToken");
+        }
+
+        public static async Task RemoveToken()
+        {
+            await _jsRuntime.InvokeVoidAsync("localStorage.removeItem", "authToken");
+        }
+    }
+}
