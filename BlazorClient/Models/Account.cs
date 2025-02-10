4a0772e3350259ece7f28717a75d7a35100399a6
Sphiwe Ndou <sphiwe.ndou@siemens.com>
2025-02-10 15:12:02 +0200

    Add Account model

---
+++ BlazorClient/Models/Account.cs
@@ -0,0 +1,9 @@
+﻿namespace BlazorClient.Models
+{
+    public class Account
+    {
+        int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Balance { get; set; }
+    }
+}
