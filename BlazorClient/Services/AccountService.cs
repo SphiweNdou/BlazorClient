4eb75df35c6fa3ad7040c137c7ae59adc2083b2f
Sphiwe Ndou <sphiwe.ndou@siemens.com>
2025-02-10 15:12:39 +0200

    Add Account Service

---
+++ BlazorClient/Services/AccountService.cs
@@ -0,0 +1,44 @@
+﻿using System.Net.Http.Headers;
+using System.Net.Http.Json;
+using BlazorClient.Helpers;
+using BlazorClient.Models;
+
+namespace BlazorClient.Services
+{
+    public class AccountService
+    {
+        private readonly HttpClient _http;
+
+        public AccountService(HttpClient http)
+        {
+            _http = http;
+        }
+
+        private async Task SetAuthorizationHeader()
+        {
+            var token = await TokenStorageHelper.GetToken();
+            if(! string.IsNullOrEmpty(token))
+            {
+                _http.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+            }
+        }
+        
+        public async Task<List<Account>> GetAccountsAsync()
+        {
+            await SetAuthorizationHeader();
+            return await _http.GetFromJsonAsync<List<Account>>("api/accounts");
+        }
+
+        public async Task CreateAccountAsync(Account account)
+        {
+            await SetAuthorizationHeader();
+            await _http.PostAsJsonAsync("api/accounts", account);
+        }
+
+        public async Task DeleteAccountAsync(int id)
+        {
+            await SetAuthorizationHeader();
+            await _http.DeleteAsync($"api/accounts/{id}");
+        }
+    }
+}
